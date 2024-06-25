6cf19a040a7b8b5c8de39778e866e97bd2c82316
geophf <dauclair@hotmail.com>
2024-06-25 14:36:21 -0400

    Adding colors as a plug-in file

---
+++ charts/d3/colors.js
@@ -0,0 +1,14 @@
+colors = {
+  ema20: {
+    light: "#fb9a99",
+    dark: "#e31a1c"
+  },
+  ratio: {
+    light: "#a6cee3",
+    dark: "#1f78b4"
+  },
+   Carrots: {
+    light: "#fdbf6f",
+    dark: "#ff7f00"
+  },
+};
