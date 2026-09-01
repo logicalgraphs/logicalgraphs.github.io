64fda059a6d2e7939b1a7da7a33edfc1e70d8473
logicalgraphs <logicalgraphs@gmail.com>
2026-09-01 22:13:45 +0000

    hwaet 2026-09-01

--- libs/pool-health.js
+++ libs/pool-health.js
@@ -1,13 +1,13 @@
 const poolHealth = {
-   generated: '2026-08-31',
+   generated: '2026-09-01',
    pools: [
       { pool: "LTC+LINK", available: '$1.27' },
-      { pool: "ETH+USDC", available: '$1.61' },
-      { pool: "BTC+USDC", available: '$7.37' },
+      { pool: "BNB+LINK", available: '$7.76' },
+      { pool: "ETH+USDC", available: '$16.44' },
       { pool: "AVAX+USDC", available: '$25.77' },
-      { pool: "BNB+LINK", available: '$60.56' },
       { pool: "ETH+BNB", available: '$66.14' },
-      { pool: "BTC+AVAX", available: '$92.99' },
+      { pool: "BTC+AVAX", available: '$93.67' },
+      { pool: "BTC+USDC", available: '$101.68' },
       { pool: "BTC+ETH", available: '$178.49' },
       { pool: "ETH+AVAX", available: '$335.41' },
       { pool: "BNB+LTC", available: '$1761.75' },
